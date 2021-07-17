26324213a90629c2c05314fa4828602bd0e2d078
Alexander <extsand@gmail.com>
2021-07-17 18:29:16 +0300

    Update application.py

--- application.py
+++ application.py
@@ -106,7 +106,9 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Flask-app CI/CD from GitHub Actions</h1>
+    <h1>Flask-app 
+	<br>
+		CI/CD from GitHub Actions</h1>
     <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
